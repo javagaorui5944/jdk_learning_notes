ed48bed422b8a5a7f827e87b730b6fc9b9d34d44
gaorui <gaorui@gaoruideMacBook-Air.local>
2016-09-06 14:56:33 +0800

    learn threadpool

---
+++ src/com/gaorui/blockingqueue/myhehe.java
@@ -0,0 +1,7 @@
+package com.gaorui.blockingqueue;
+
+/**
+ * Created by gaorui on 16/9/5.
+ */
+public class myhehe {
+}
---
+++ src/com/gaorui/threadpool/Test.java
@@ -0,0 +1,44 @@
+package com.gaorui.threadpool;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by gaorui on 16/9/6.
+ */
+public class Test {
+    //测试主函数
+    public static void main(String[] args) {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 200, TimeUnit.MILLISECONDS,
+                new ArrayBlockingQueue<Runnable>(5));
+
+        for(int i=0;i<15;i++){
+            MyTask myTask = new MyTask(i);
+            executor.execute(myTask);
+            System.out.println("线程池中线程数目："+executor.getPoolSize()+"，队列中等待执行的任务数目："+
+                    executor.getQueue().size()+"，已执行玩别的任务数目："+executor.getCompletedTaskCount());
+        }
+        executor.shutdown();
+    }
+}
+
+//线程类
+class MyTask implements Runnable {
+    private int taskNum;
+
+    public MyTask(int num) {
+        this.taskNum = num;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("正在执行task "+taskNum);
+        try {
+            Thread.currentThread().sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("task "+taskNum+"执行完毕");
+    }
+}
